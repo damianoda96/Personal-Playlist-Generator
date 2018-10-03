a55b44ef790e1541ad6ffb265a66acc0a6557c97
Deven Damiano <damiano.da96@gmail.com>
2018-10-03 17:05:11 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Semester Project for AI - Deven Damiano - dad152@zips.uakron.edu - Nicholas Horvath - nch16@zips.uakron.edu
+
+import sys
+import numpy as np
+import pandas as pd
+from sklearn import tree
+from sklearn import datasets
+from sklearn import svm
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.externals import joblib
+
+print("Hello World")
